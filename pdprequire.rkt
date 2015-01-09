2805a09f7441e346798bdd2e6982c2c2aade85f5
Daniel Feltey <dfeltey@gmail.com>
2015-01-09 12:56:51 -0500

    simplify definition of pdp-require macro

--- pdprequire.rkt
+++ pdprequire.rkt
@@ -2,47 +2,31 @@
 
 (provide pdp-require)
 
-(require (for-syntax syntax/parse
-                     racket/syntax))
+(require (for-syntax syntax/parse racket/syntax))
 (require (only-in rackunit dynamic-require/expose))
 
-(begin-for-syntax 
-  (define-syntax-class pdp-require-form
-    (pattern name:id
-             #:with sym #'(quote name)
-             #:with def #'name)
-    (pattern (orig-name:id new-name:id)
-             #:with sym #'(quote orig-name)
-             #:with def #'new-name))
-  
-  (define-splicing-syntax-class keywords
-    (pattern 
-     (~seq 
-      (~or (~optional (~seq #:default de:expr)
-                      #:name "#:default option"
-                      #:defaults ([de (lambda (name)
-                                        #`(error 'pdp-require "~a not provided" (quote #,name)))]))
-           (~optional (~seq #:prefix pf:id)
-                      #:name "#:prefix option"
-                      #:defaults ([pf ""]))) ...)
-     #:attr apply-prefix (lambda (stx name) (format-id stx "~a~a" (attribute pf) name))
-     #:attr default-value #'de)))
+(define (dynamic-require/expose/safe mod name)
+  (with-handlers ([exn:fail:contract:variable? (lambda (exn) (lambda args ((error (format "function ~a is not defined" name)))))]
+                  [exn:fail:filesystem? (lambda (exn) (lambda args (error (format "module ~a does not exist" mod))))])
+    (dynamic-require/expose mod name)))
 
-;; NOTE: This should mostly work, an exception is thrown when the "required" module
-;;            does not exist, which should be a favorable behavior.
-;;
-;; TODO: 
-;;       1. Better handling of the default values in order to have good error messages
+(begin-for-syntax
+  (define-syntax-class pdp-require-form
+    #:literals (prefix-in)
+    (pattern (pdp-require mod-exp fn:id ...+) 
+             #:with mod #'mod-exp
+             #:with req-ids #'((quote fn) ...)
+             #:with def-ids #'(fn ...))
+    (pattern (pdp-require (prefix-in pf:id mod-exp fn:id ...+))
+             #:with mod #'mod-exp
+             #:with req-ids #'((quote fn) ...)
+             #:with def-ids (map (lambda (id) (format-id id "~a~a" #'pf id)) 
+                                 (syntax->list #'(fn ...))))))
+
 (define-syntax (pdp-require stx)
-  (syntax-parse stx
-    [(_ mod name:pdp-require-form ... kw:keywords)
-     (define apply-prefix (attribute kw.apply-prefix))
-     (define default-value (attribute kw.default-value))
-     #`(begin
-         #,@(for/list ([def-name (syntax->list #'(name.def ...))]
-                       [req-name (syntax->list #'(name.sym ...))])
-              (define name (apply-prefix stx def-name))
-              #`(define #,name 
-                  (with-handlers ([exn:fail:contract:variable? (lambda (exn) #,default-value)])
-                    (dynamic-require/expose mod #,req-name)))))]))
-    
+  (syntax-parse stx 
+    [prf:pdp-require-form
+     (with-syntax ([(def ...) #'prf.def-ids]
+                   [(req ...) #'prf.req-ids])
+       #'(begin
+           (define def (dynamic-require/expose/safe prf.mod req)) ...))]))
